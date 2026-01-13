a4b27d1de6f44d601ce0151f3f286617b28a097d
Marius Volkhart <marius@volkhart.com>
2026-01-13 10:08:26 -0500

    Improve build scripts

--- buildSrc/src/main/kotlin/com/pkware/gradle/KotlinConventionsPlugin.kt
+++ buildSrc/src/main/kotlin/com/pkware/gradle/KotlinConventionsPlugin.kt
@@ -2,19 +2,23 @@ package com.pkware.gradle
 
 import com.diffplug.gradle.spotless.SpotlessExtension
 import com.diffplug.gradle.spotless.SpotlessPlugin
+import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.DetektPlugin
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.plugins.JavaLibraryPlugin
 import org.gradle.api.plugins.jvm.JvmTestSuite
-import org.gradle.api.tasks.bundling.AbstractArchiveTask
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.invoke
+import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.withType
 import org.gradle.testing.base.TestingExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 /**
  * Plugin for projects that support Kotlin.
@@ -46,7 +50,19 @@ class KotlinConventionsPlugin : Plugin<Project> {
       config.from("$rootDir/detekt.yml")
     }
     configurations.named("detektPlugins") {
-      dependencies.add(project.dependencies.create("com.pkware.detekt:import-extension:1.2.0"))
+      dependencies.add(libs.findLibrary("detekt-imports").get().get())
+    }
+    val buildDirectory = layout.buildDirectory.get()
+    val generatedFilesWindows = "$buildDirectory\\generated"
+    val generatedFilesUnix = "$buildDirectory/generated"
+    // configureEach for lazy configuration
+    tasks.withType<Detekt>().configureEach {
+      jvmTarget = tasks.named<KotlinCompile>("compileKotlin").get().compilerOptions.jvmTarget.get().target
+      exclude {
+        // Work around https://github.com/detekt/detekt/issues/4743
+        val absolutePath = it.file.absolutePath
+        absolutePath.startsWith(generatedFilesUnix) || absolutePath.startsWith(generatedFilesWindows)
+      }
     }
 
     configure<TestingExtension> {
@@ -55,6 +71,11 @@ class KotlinConventionsPlugin : Plugin<Project> {
           implementation(libs.findLibrary("assertk").get().get().toString())
         }
       }
+    }
+
+    // If the project is a Kotlin library, activate explicit API mode.
+    plugins.withType<JavaLibraryPlugin> {
+      extensions.getByType<KotlinJvmProjectExtension>().explicitApi()
     }
   }
 }
